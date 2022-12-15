377bcc5b23d5a5250ca1afb9ca8a0dd4b560c508
Alozie winnie <mywinnie23@gmail.com>
2022-12-15 10:57:50 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,9 +5,9 @@
  * @c: input
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 'A', c <= 'Z', c++)
 		return (1);
 	else
 		return (0);

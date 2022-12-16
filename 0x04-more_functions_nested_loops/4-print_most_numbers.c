7efde567e060f776f3eb6f2db5ef64fa4c4d424c
Alozie winnie <mywinnie23@gmail.com>
2022-12-16 12:50:21 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,18 @@
 #include "main.h"
+
 /**
- * print_most_numbers - check the code for Holberton School students.
- * Return: Always 0.
- */
+ *print_most_numbers - a function that prints the numbers, from 0 to 9
+ *Return 0-9, excluding 2 and 4, followed by a new line
+*/
 void print_most_numbers(void)
 {
-	char c = '0';
-
-	while (c <= '9')
-	{
-		if (c != '2' && c != '4')
-		{
-			_putchar(c);
-		}
-	c++;
-	}
+int i;
+for (i = 0; i <= 9; i++)
+{
+if ((i == 2) || (i == 4))
+continue;
+else
+_putchar(i + '0');
+}
 _putchar('\n');
 }

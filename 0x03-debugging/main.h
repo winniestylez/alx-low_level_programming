7813d0a589821d5c34fd15f7d3d8788352d19999
Alozie winnie <mywinnie23@gmail.com>
2022-12-16 10:59:19 -0800

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,10 @@
-#ifndef main_h
-#define main_h
-extern int _putchar(char c);
-extern void print_alphabet(void);
-extern void print_alphabet_x10(void);
-extern int _islower(int c);
-extern int _isalpha(int c);
-extern int print_sign(int n);
-extern int _abs(int);
-extern int print_last_digit(int);
-extern void jack_bauer(void);
-extern void times_table(void);
-extern int add(int, int);
-extern void print_to_98(int n);
-extern void print_times_table(int n);
-#endif
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */

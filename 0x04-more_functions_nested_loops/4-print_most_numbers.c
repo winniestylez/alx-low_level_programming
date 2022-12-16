e6a09acf6653fd6a2e6a7f994d2b215980e2e972
Alozie winnie <mywinnie23@gmail.com>
2022-12-16 13:10:02 -0800

    4print_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,10 @@ void print_most_numbers(void)
 int i;
 for (i = 0; i <= 9; i++)
 {
-if ((i != 2) || (i != 4))
+if ((i == 2) || (i == 4))
 continue;
 else
-_putchar(i + '0');
+_putchar(i % 10 + '0');
 }
 _putchar('\n');
 }

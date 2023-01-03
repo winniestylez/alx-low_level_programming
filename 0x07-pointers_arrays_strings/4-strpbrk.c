079719125940ca884d9c2a14f6a4a7f96940232f
Alozie winnie <mywinnie23@gmail.com>
2023-01-02 20:54:36 -0800

    4-sbdbdb

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,24 +1,35 @@
-#include <stdio>
 #include "main.h"
 
 /**
- * _strpbrk - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
- */
+  * _strpbrk - search a string for any of a set of bytes
+  * @s: source string
+  * @accept: accepted characters
+  *
+  * Return: the string since the first found accepted character
+  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
 
-	for (i = 0; s[i] != '\0'; i++)
+	int a = 0, b;
+
+	while (s[a])
 	{
-		for (n = 0; accept[n] != '\0'; n++)
+		b = 0;
+
+		while (accept[b])
 		{
-			if (s[i] == accept[n])
-				return (s + i);
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+
+			b++;
 		}
+
+		a++;
 	}
 
-	return (NULL);
+	return ('\0');
 }
+

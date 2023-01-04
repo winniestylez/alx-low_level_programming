3a29a541b603e5e2f202365100f7142270b4d4ce
Alozie winnie <mywinnie23@gmail.com>
2023-01-04 10:01:17 -0800

    /**  * _puts_recursion - function like puts();  * @s: input  * Return: Always 0 (Success)  */

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+
+int i = 0;
+void _puts_recursion(char *s){
+	_putchar("print a string %c\n", i);
+
+	
+		return 0;
+}

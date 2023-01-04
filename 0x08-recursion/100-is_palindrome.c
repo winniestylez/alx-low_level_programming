2821c7b94eb2b7c8efb2101f43c7fc58a084da41
Alozie winnie <mywinnie23@gmail.com>
2023-01-04 11:41:10 -0800

    gshsvd

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+ #include <string.h>
+ 
+ 
+int checkpalindrome(char *s)
+{
+    static int i,c=0,n=strlen(s);
+    if(i<n/2)
+    {
+         if(s[i]==s[n-i-1])
+    	c++;
+    	i++;
+    	checkpalindrome(s);
+	}
+	else
+	{
+		if(c==i)
+        return 1;
+        else
+        return 0;
+	}
+ 	 
+ 	
+}
+int main()
+{
+    char s[1000];  
+   
+    printf("Enter  the string: ");
+    gets(s);
+    
+ 
+    if(checkpalindrome(s))
+ 	    printf("string is palindrome");
+    else
+        printf("string is not palindrome");
+ 
+ }

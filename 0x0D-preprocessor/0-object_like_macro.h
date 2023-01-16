758ad9fe355501abc8e5fa6d8291540df46c0295
Alozie winnie <mywinnie23@gmail.com>
2023-01-16 08:46:56 -0800

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_MACRO_H
+#define OBJECT_MACRO_H
+
+/**
+ * file - 0-object_like_macro.h
+ * Description: A header that defines a macro SIZE abbreviated
+ * as a token - 1024
+ */
+
+#define SIZE (1024)
+
+#endif

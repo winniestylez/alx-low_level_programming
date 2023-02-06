2181e2a5a0b9d94d6072a3d22f435f46a29674dd
Alozie Winnie <winniechisom@gmail.com>
2023-02-06 15:00:36 -0800

    dfgg

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,28 @@
 #include "main.h"
 
 /**
-  * create_file - ...
-  * @filename: ...
-  * @text_content: ...
-  *
-  * Return: ...
-  */
+ * * create_file - creates a file and fills it with text
+ * * @filename: name of the file to create
+ * * @text_content: text to write in the file
+ * *
+ * * Return: 1 on success, -1 on failure
+*/
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-
-	if (!filename)
-		return (-1);
-
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
-		write(fd, text_content, _strlen(text_content));
-
-	close(fd);
-	return (1);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
+int fd, x, y = 0;
+if (!filename)
+return (-1);
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd < 0)
+return (-1);
+if (text_content)
 {
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
+while (text_content[y])
+y++;
+x = write(fd, text_content, y);
+if (x != y)
+return (-1);
+}
+close(fd);
+return (1);
 }
